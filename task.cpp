5a3e26014c405122f449bc0bb5a3194ff52df8b1
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-04-04 19:49:41 +0300

    Fix dacf definition

--- task.cpp
+++ task.cpp
@@ -33,8 +33,10 @@ void dacf(cmplx *in, cmplx *out, size_t sz)
 {
 	for (size_t k = 0; k < sz; ++k) {
 		out[k] = 0;
-		for (size_t n = k; n < sz; ++n)
-			out[k] += in[n] * std::conj(in[n-k]);
+		for (size_t n = 0; n < sz - k; ++n)
+			out[k] += in[n] * std::conj(in[n+k]);
+		for (size_t n = sz - k; n < sz; ++n)
+			out[k] += in[n] * std::conj(in[n+k-sz]);
 	}
 }
 
@@ -56,32 +58,19 @@ void sig_abs(cmplx *in, float *out, size_t sz)
 		out[k] = std::abs(in[k]);
 }
 
-void sig_sqrt(float *arr, size_t sz)
+void sig_sqrt(cmplx *arr, size_t sz)
 {
 	for (size_t k = 0; k < sz; ++k)
 		arr[k] = std::sqrt(arr[k]);
 }
 
-void rectangle_impulse(cmplx *out, size_t sz)
-{
-	size_t n1, n2;
-	n1 = sz / 2 - sz / 8;
-	n2 = sz / 2 + sz / 8;
-	for (size_t n = 0; n < n1; ++n)
-		out[n] = 0;
-	for (size_t n = n1; n < n2; ++n)
-		out[n] = 1;
-	for (size_t n = n2; n < sz; ++n)
-		out[n] = 0;
-}
-
-void triangle_sequence(cmplx *out, size_t sz, float len)
+void triangle_sequence(cmplx *out, size_t sz, size_t len)
 {
 	for (size_t n = 0; n < sz; ++n) {
-		long disc = long(float(n) / len);
+		long disc = long(n / len);
 		if (disc % 1 == 0) {
 			float x = float(n) - disc * len;
-			x = x / len - 0.5;
+			x = x / (len - 1) - 0.5;
 			out[n] = cmplx(1 - 2*std::abs(x), 0);
 		} else {
 			out[n] = cmplx(0, 0);
@@ -95,15 +84,15 @@ void out_to_file(char const *path, float *v,
 	std::ofstream file;
 	file.open(path);
 	for (size_t i = from; i < to; ++i)
-		file << v[i] << "\n";
+		file << i << " " << v[i] << "\n";
 	file.close();
 }
 
 int main()
 {
-	size_t mult = 8;		/* mult = 8 was used */
+	size_t mult = 2;		/* Adjust number of points */
 	size_t sz = 1024 * mult;	/* Number of points in signal buf */
-	float len = 64;		 	/* Single triangle impulse lenght */
+	int len = 64 * mult;		/* Single triangle impulse lenght */
 
 	auto s_signal = new cmplx[sz];
 	auto s_buf1   = new cmplx[sz];
@@ -114,19 +103,19 @@ int main()
 	triangle_sequence(s_signal, sz, len);		// s = triangle seq.
 	dft(s_signal, s_buf1, sz);			// Apply dft
 	sig_abs(s_buf1, s_result, sz);			// Aplly abs
-	out_to_file("./out/abs_dft.dat", s_result, 0, 128 * mult);
+	out_to_file("./out/abs_dft.dat", s_result, 0, 512 * mult);
 
 	/* Check dft_inv(dft(s)) ?= s */
 	dft_inv(s_buf1, s_buf2, sz);			// dft_inv(dft))
 	sig_abs(s_buf2, s_result, sz);			// Apply abs
-	out_to_file("./out/dftinv_dft.dat", s_result, 0, 128 * mult);
+	out_to_file("./out/dftinv_dft.dat", s_result, 0, 256 * mult);
 
 	/* Main task */
 	dacf(s_signal, s_buf1, sz);			// Apply dacf
 	dft(s_buf1, s_buf2, sz);			// Apply dft
+	sig_sqrt(s_buf2, sz);				// Apply sqrt
 	sig_real(s_buf2, s_result, sz);			// Apply real
-	sig_sqrt(s_result, sz);				// Apply sqrt
-	out_to_file("./out/sqrt_dft_dacf.dat", s_result, 0, 128 * mult);
+	out_to_file("./out/sqrt_dft_dacf.dat", s_result, 0, 512 * mult);
 
 	sig_real(s_buf1, s_result, sz);			// Get Re(dacf)
 	out_to_file("./out/real_dacf.dat", s_result, 0, 128 * mult);
